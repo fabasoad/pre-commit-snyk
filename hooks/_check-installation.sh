22f220c3a57cb6ccee704b254afb21fd7ebc7ea0
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2021-11-08 15:33:34 +0900

    Change installation to npm (#6)

--- hooks/_check-installation.sh
+++ hooks/_check-installation.sh
@@ -3,20 +3,5 @@ set -eu
 
 if ! command -v snyk &> /dev/null
 then
-  echo "snyk is not installed"
-  if [ "$(uname -s)" = "Darwin" ]; then
-     os="macos"
-  elif [[ $(which apk) ]]; then
-     os="alpine"
-  elif [[ $(which apt) ]]; then
-     os="linux"
-  else
-     os="win"
-  fi
-
-  INSTALLATION_DIR="${HOME}"/bin
-  mkdir -p "${INSTALLATION_DIR}"
-  curl -s https://static.snyk.io/cli/latest/release.json | jq ".assets.\"snyk-${os}\".url" | xargs curl --output "${INSTALLATION_DIR}"/snyk
-  chmod +x "${INSTALLATION_DIR}"/snyk
-  export PATH="${INSTALLATION_DIR}/:$PATH"
+  npm install snyk@latest -g
 fi
