dec45394f63c5a480612d0060dd8df33cfeb4e01
fabasoad <fabasoad@gmail.com>
2021-11-11 16:40:36 +0900

    Add args support for  and

--- hooks/snyk-container.sh
+++ hooks/snyk-container.sh
@@ -5,4 +5,4 @@ bash "${SCRIPT_DIR}"/_check-installation.sh
 name=$(openssl rand -hex 4)
 tag=$(openssl rand -hex 4)
 docker build -t "${name}:${tag}" .
-snyk container test "${name}:${tag}"
+snyk container test "${name}:${tag}" "$@"
--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -2,4 +2,4 @@
 set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/_check-installation.sh
-snyk test
+snyk test "$@"
