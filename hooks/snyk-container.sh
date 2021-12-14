bdded8612875b849093f9dbee58a6371b4f621d6
fabasoad <fabasoad@gmail.com>
2021-12-14 11:57:55 +0900

    Fix script

--- hooks/snyk-container.sh
+++ hooks/snyk-container.sh
@@ -5,12 +5,14 @@ bash "${SCRIPT_DIR}"/_check-installation.sh
 
 snyk_args=()
 dockerfiles=()
-while [[ $# -gt 0 ]]; do
-  if [[ $1 == *Dockerfile ]]; then
-    dockerfiles+=("$1")
+i=1
+for arg in "$@"; do
+  if [[ $arg == *Dockerfile ]]; then
+    dockerfiles+=("$arg")
   else
-    snyk_args+=("$1")
+    snyk_args+=("$arg")
   fi
+  i=$((i + 1))
 done
 
 prefix="[pre-commit-snyk]"
@@ -18,21 +20,18 @@ prefix="[pre-commit-snyk]"
 tag=$(date +%s)
 i=1
 
-echo $dockerfiles
-echo $snyk_args
-
-#for file_path in $dockerfiles; do
-#  printf "%s %s\n" "$prefix" "$file_path"
-#  image="pre-commit-snyk:$tag-$i"
-#  if [[ $i -gt 1 ]]
-#  then
-#    echo ""
-#  fi
-#  printf "%s Building %s from %s\n\n" "$prefix" "$image" "$file_path"
-#  docker build -t "$image" "$(echo "$file_path" | rev | cut -d'/' -f2- | rev)"
-#  printf "\n%s Testing %s\n\n" "$prefix" "$image"
-#  snyk container test "$image" "--file=$file_path" "$@"
-#  printf "\n%s Removing %s\n\n" "$prefix" "$image"
-#  docker rmi "$(docker images "$image" -q)" || printf "\n%s Unable to remove %s" "$prefix" "$image"
-#  i=$((i + 1))
-#done
+for file_path in "${dockerfiles[@]}"; do
+  printf "%s %s\n" "$prefix" "$file_path"
+  image="pre-commit-snyk:$tag-$i"
+  if [[ $i -gt 1 ]]
+  then
+    echo ""
+  fi
+  printf "%s Building %s from %s\n\n" "$prefix" "$image" "$file_path"
+  docker build -t "$image" "$(echo "$file_path" | rev | cut -d'/' -f2- | rev)"
+  printf "\n%s Testing %s\n\n" "$prefix" "$image"
+  snyk container test "$image" "--file=$file_path" "${snyk_args[*]}"
+  printf "\n%s Removing %s\n\n" "$prefix" "$image"
+  docker rmi "$(docker images "$image" -q)" || printf "\n%s Unable to remove %s" "$prefix" "$image"
+  i=$((i + 1))
+done
