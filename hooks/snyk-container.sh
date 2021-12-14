5ee898943debf86829e8822f2e08c41e3a7d435e
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2021-12-14 12:08:09 +0900

    Add possibility to exclude file (#12)
    * Fix snyk container
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Fix script
    
    * Fix script
    
    * Fix script
    
    * Fix script

--- hooks/snyk-container.sh
+++ hooks/snyk-container.sh
@@ -3,12 +3,24 @@ set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/_check-installation.sh
 
+snyk_args=()
+dockerfiles=()
+i=1
+for arg in "$@"; do
+  if [[ $arg == *Dockerfile ]]; then
+    dockerfiles+=("$arg")
+  else
+    snyk_args+=("$arg")
+  fi
+  i=$((i + 1))
+done
+
 prefix="[pre-commit-snyk]"
 
 tag=$(date +%s)
 i=1
-# shellcheck disable=SC2044
-for file_path in $(find . -type f -name "Dockerfile"); do
+
+for file_path in "${dockerfiles[@]}"; do
   image="pre-commit-snyk:$tag-$i"
   if [[ $i -gt 1 ]]
   then
@@ -16,9 +28,9 @@ for file_path in $(find . -type f -name "Dockerfile"); do
   fi
   printf "%s Building %s from %s\n\n" "$prefix" "$image" "$file_path"
   docker build -t "$image" "$(echo "$file_path" | rev | cut -d'/' -f2- | rev)"
-  printf "\n%s Testing %s\n\n" "$prefix" "$image"
-  snyk container test "$image" "--file=$file_path" "$@"
-  printf "\n%s Removing %s\n\n" "$prefix" "$image"
+  printf "\n%s Testing %s\n" "$prefix" "$image"
+  snyk container test "$image" "--file=$file_path" "${snyk_args[*]}"
+  printf "\n%s Removing %s" "$prefix" "$image"
   docker rmi "$(docker images "$image" -q)" || printf "\n%s Unable to remove %s" "$prefix" "$image"
   i=$((i + 1))
 done
