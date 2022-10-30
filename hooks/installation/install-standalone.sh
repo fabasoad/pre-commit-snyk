25431b82a844bf87bc0437c82d4e6f3ab6bdf0f5
fabasoad <fabasoad@gmail.com>
2022-10-31 00:28:00 +0900

    Debug 3

--- hooks/installation/install-standalone.sh
+++ hooks/installation/install-standalone.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -eu
 
-ext=""
 case "$(uname -s)" in
   Darwin*)
     binary="snyk-macos"
     ;;
   MINGW64*)
     binary="snyk-win.exe"
-    ext=".exe"
     ;;
   MSYS_NT*)
     binary="snyk-win.exe"
-    ext=".exe"
     ;;
   *)
     if [[ "$(uname -m)" == "arm64" ]]; then
@@ -29,6 +26,6 @@ fi
 
 echo "GET https://static.snyk.io/cli/latest/$binary"
 
-curl https://static.snyk.io/cli/latest/$binary -o snyk$ext
-chmod +x ./snyk$ext
+curl https://static.snyk.io/cli/latest/$binary -o snyk
+chmod +x ./snyk
 PATH=$PATH:$(pwd)
