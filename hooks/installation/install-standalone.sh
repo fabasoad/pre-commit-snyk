f25c3dc83751ea3644dbf3b855b0cdf5c743a055
fabasoad <fabasoad@gmail.com>
2022-10-31 00:48:50 +0900

    Debug 14

--- hooks/installation/install-standalone.sh
+++ hooks/installation/install-standalone.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -eu
 
-case "$(uname -s)" in
-  Darwin*)
-    binary="snyk-macos"
-    ;;
-  MINGW64*)
-    binary="snyk-win.exe"
-    ;;
-  MSYS_NT*)
-    binary="snyk-win.exe"
-    ;;
-  *)
-    if [[ "$(uname -m)" == "arm64" ]]; then
-      binary="snyk-linux-arm64"
-    else
-      binary="snyk-linux"
-    fi
-    ;;
-esac
-
-if [[ "$(uname -a)" == *"alpine"* ]]; then
+if [[ -f "/etc/alpine-release" ]]; then
   binary="snyk-alpine"
+else
+  case "$(uname -s)" in
+    Darwin*)
+      binary="snyk-macos"
+      ;;
+    MINGW64*)
+      binary="snyk-win.exe"
+      ;;
+    MSYS_NT*)
+      binary="snyk-win.exe"
+      ;;
+    *)
+      if [[ "$(uname -m)" == "arm64" ]]; then
+        binary="snyk-linux-arm64"
+      else
+        binary="snyk-linux"
+      fi
+      ;;
+  esac
 fi
 
 echo "GET https://static.snyk.io/cli/latest/$binary"
