fcd9a40280e9471393b0a0853df85147c27b22c2
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 15:12:37 +0100

    Improving hooks to pass if file is not detected

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -3,12 +3,9 @@ set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
-printf "Executing code-test hook\n" >&2
-
 # Check for the presence of code files
 if ! ls *.js *.ts *.java *.py &> /dev/null; then
-  printf "Warning: No code files detected. Skipping Snyk code test.\n" >&2
-  exit 0
+  echo "Warning: No code files detected. Skipping Snyk code test."
+else
+  snyk code test "$@"
 fi
-
-snyk code test "$@"
--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -3,12 +3,9 @@ set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
-printf "Executing test hook\n" >&2
-
 # Check for the presence of test files
 if ! ls *test.js *test.ts *test.py &> /dev/null; then
-  printf "Warning: No test files detected. Skipping Snyk test.\n" >&2
-  exit 0
+  echo "Warning: No test files detected. Skipping Snyk test."
+else
+  snyk test "$@"
 fi
-
-snyk test "$@"
