9fd187dffafe4c711da76f0d4e93c456775cc47a
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 15:07:07 +0100

    Improving hooks to pass if file is not detected

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -3,7 +3,7 @@ set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
-echo "Executing code-test hook"
+echo "Executing code-test hook" >&2
 
 # Check for the presence of code files
 if ! ls *.js *.ts *.java *.py &> /dev/null; then
--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -3,7 +3,7 @@ set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
-echo "Executing test hook"
+echo "Executing test hook" >&2
 
 # Check for the presence of test files
 if ! ls *test.js *test.ts *test.py &> /dev/null; then
