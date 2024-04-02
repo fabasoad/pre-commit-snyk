9439b74a0319edd9ef6f7347c115a1f408be6583
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 15:34:01 +0100

    Improving hooks to pass if file is not detected

--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of test files
-if ! ls *test.js *test.ts *test.py &> /dev/null; then
+if ! ls *.js *.ts *.py &> /dev/null; then
   echo "Warning: No test files detected. Skipping Snyk test."
 else
   snyk test "$@"
