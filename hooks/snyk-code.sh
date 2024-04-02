5cf73c8aaa326f71949856f195a03e950b22bd90
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 15:43:25 +0100

    Improving hooks to pass if file is not detected

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of code files
-if ! ls *.js *.ts *.java *.py &> /dev/null; then
+if ls *.js *.ts *.java *.py &> /dev/null; then
   snyk code test "$@"
 else
   echo "Warning: No code files detected. Skipping Snyk code test."
--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of IaC files
-if ! ls *.tf *.yaml *.yml &> /dev/null; then
+if ls *.tf *.yaml *.yml &> /dev/null; then
   snyk iac test "$@"
 else
-  echo "Warning: No code files detected. Skipping Snyk code test."
+  echo "Warning: No IaC files detected. Skipping Snyk IaC test."
+fi
+
