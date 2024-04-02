2129fcf47e9d612a0c46ebdd940bb672d1e3d6d2
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 15:00:21 +0100

    Adding echo to snyk-test.sh to explain missing file error

--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -4,8 +4,9 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of test files
-if ! ls *test.js *test.ts *test.py &> /dev/null; then
-  echo "Warning: No test files detected. Skipping Snyk test."
-else
-  snyk test "$@"
+if ! ls *.js *.ts *.java *.py &> /dev/null; then
+  echo "Warning: No code files detected. Skipping Snyk code test."
+  exit 0
 fi
+
+snyk code test "$@"
