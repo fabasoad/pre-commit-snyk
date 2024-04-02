185fc9991d5489ae8f8c862d682ad597ddde5f6a
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 16:36:48 +0100

    Adding additional extensions to snyk-iac.sh & snyk-code.sh

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of code files
-if ls *.js *.ts *.java *.py &> /dev/null; then
+if ls *.js *.jsx *.mjs *.ts *.tsx *.java *.py *.rb *.go *.kt *.kts *.swift *.php *.rs *.c *.h *.cpp *.cxx *.cc *.hpp *.hxx *.cs *.scala *.pl *.pm *.sh *.bash *.zsh *.html *.htm *.css *.yaml *.yml *.json &> /dev/null; then
   snyk code test "$@"
 else
   echo "Warning: No code files detected. Skipping Snyk code test."
--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of IaC files
-if ls *.tf *.yaml *.yml &> /dev/null; then
+if ls *.tf *.tf.json *.tfvars *.yaml *.yml *.json *.template &> /dev/null; then
   snyk iac test "$@"
 else
   echo "Warning: No IaC files detected. Skipping Snyk IaC test."
