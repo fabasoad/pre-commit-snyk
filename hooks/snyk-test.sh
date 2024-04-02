08252c4254475ddd2e835e3e597a73564a2a3c09
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-02 15:53:57 +0100

    Improving hooks to pass if file is not detected

--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -3,4 +3,10 @@ set -eu
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
-snyk test "$@"
+# Check for the presence of dependency files
+if ls package.json requirements.txt &> /dev/null; then
+  snyk test "$@"
+else
+  echo "Warning: No dependency files detected. Skipping Snyk dependency test."
+fi
+
