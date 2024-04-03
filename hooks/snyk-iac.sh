b2576e6f520ab5e26572e7283f9a214d705fd72d
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-03 12:03:41 +0100

    Modifying snyk bash files to look for specific file extensions

--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of infrastructure as code files
-FIND_RESULT=$(find . \( -name '*.tf' -o -name '*.hcl'  -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' -o -name '*.tf.json' \) -print -quit)
+FIND_RESULT=$(find . \( -name '*.tf' -o -name '*.hcl' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' -o -name '*.tf.json' \) -print -quit)
 
 if [ -n "$FIND_RESULT" ]; then
   echo "Matching files found: $FIND_RESULT"
