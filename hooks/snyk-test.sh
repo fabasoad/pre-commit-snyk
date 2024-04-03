b5ac1fdb95a2091efba51394b01fb0cee32c7f77
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-03 10:34:53 +0100

    Modifying snyk bash files to look for specific file extensions

--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of dependency files
-if find . \( -name 'package.json' -o -name 'package-lock.json' -o -name 'yarn.lock' -o -name 'Gemfile' -o -name 'Gemfile.lock' -o -name 'requirements.txt' -o -name 'Pipfile' -o -name 'Pipfile.lock' -o -name 'composer.json' -o -name 'composer.lock' -o -name 'go.mod' -o -name 'go.sum' -o -name 'Gopkg.toml' -o -name 'Gopkg.lock' -o -name 'build.gradle' -o -name 'pom.xml' -o -name 'Cargo.toml' -o -name 'Cargo.lock' \) -print -quit; then
+if find . \( -name 'package.json' -o -name 'package-lock.json' -o -name 'yarn.lock' -o -name 'Gemfile' -o -name 'Gemfile.lock' -o -name 'requirements.txt' -o -name 'Pipfile' -o -name 'Pipfile.lock' -o -name 'composer.json' -o -name 'composer.lock' -o -name 'go.mod' -o -name 'go.sum' -o -name 'Gopkg.toml' -o -name 'Gopkg.lock' -o -name 'build.gradle' -o -name 'pom.xml' -o -name 'Cargo.toml' -o -name 'Cargo.lock' \) -print; then
   snyk test "$@"
 else
   echo "Warning: No supported dependency files detected. Skipping Snyk dependency test."
