fa2f74437cc470a578a53f3f678e5fa9f0f66d80
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-03 09:34:12 +0100

    Modifying snyk bash files to look for specific file extensions

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of code files
-if find . \( -name '*.js' -o -name '*.jsx' -o -name '*.ts' -o -name '*.tsx' -o -name '*.java' -o -name '*.py' -o -name '*.rb' -o -name '*.go' -o -name '*.kt' -o -name '*.kts' -o -name '*.swift' -o -name '*.rs' -o -name '*.scala' -o -name '*.sh' -o -name '*.html' -o -name '*.css' -o -name '*.yaml' -o -name '*.yml' -o -name '*.json' \) -print -quit &>/dev/null; then
+if find . \(-o -name '*.js' -o -name '*.jsx' -o -name '*.ts' -o -name '*.tsx' -o -name '*.java' -o -name '*.py' -o -name '*.rb' -o -name '*.go' -o -name '*.kt' -o -name '*.kts' -o -name '*.swift' -o -name '*.rs' -o -name '*.scala' -o -name '*.sh' -o -name '*.html' -o -name '*.css' -o -name '*.yaml' -o -name '*.yml' -o -name '*.json' \) -print -quit &>/dev/null; then
   snyk code test "$@"
 else
   echo "Warning: No supported code files detected. Skipping Snyk code test."
--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of infrastructure as code files
-if find . \( -name '*.tf' -o -name '*.hcl' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' -o -name '*.tf.json' \) -print -quit &>/dev/null; then
+if find . \(-o -name '*.tf' -o -name '*.hcl' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' -o -name '*.tf.json' \) -print -quit &>/dev/null; then
   snyk iac test "$@"
 else
   echo "Warning: No supported infrastructure as code files detected. Skipping Snyk IaC test."
