5fde61c0968c042c9129b7141fabce7d4bd5dfba
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-03 10:54:13 +0100

    Modifying snyk bash files to look for specific file extensions

--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -6,6 +6,7 @@ bash "${SCRIPT_DIR}"/installation/main.sh
 # Check for the presence of infrastructure as code files
 if [ -n "$(find . \( -name '*.tf' -o -name '*.hcl' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' -o -name '*.tf.json' \) -print -quit)" ]; then
   snyk iac test "$@"
+  exit 0
 else
   echo "No supported infrastructure as code files detected. Skipping Snyk IaC test."
   exit 0  # Return success status
