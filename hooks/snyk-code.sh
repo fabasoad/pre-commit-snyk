d6769cf7dbc7529edfe0be745672370aa43240be
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-03 09:26:04 +0100

    Modifying snyk bash files to look for specific file extensions

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of code files
-if find . \( -name '*.js' -o -name '*.jsx' -o -name '*.mjs' -o -name '*.ts' -o -name '*.tsx' -o -name '*.java' -o -name '*.py' -o -name '*.rb' -o -name '*.go' -o -name '*.kt' -o -name '*.kts' -o -name '*.swift' -o -name '*.php' -o -name '*.rs' -o -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.cxx' -o -name '*.cc' -o -name '*.hpp' -o -name '*.hxx' -o -name '*.cs' -o -name '*.scala' -o -name '*.pl' -o -name '*.pm' -o -name '*.sh' -o -name '*.bash' -o -name '*.zsh' -o -name '*.html' -o -name '*.htm' -o -name '*.css' -o -name '*.yaml' -o -name '*.yml' -o -name '*.json' \) -print -quit &>/dev/null; then
+if find . \( -name '*.js' -o -name '*.jsx' -o -name '*.ts' -o -name '*.tsx' -o -name '*.java' -o -name '*.py' -o -name '*.rb' -o -name '*.go' -o -name '*.kt' -o -name '*.kts' -o -name '*.swift' -o -name '*.rs' -o -name '*.scala' -o -name '*.sh' -o -name '*.html' -o -name '*.css' -o -name '*.yaml' -o -name '*.yml' -o -name '*.json' \) -print -quit &>/dev/null; then
   snyk code test "$@"
 else
-  echo "Warning: No code files detected. Skipping Snyk code test."
+  echo "Warning: No supported code files detected. Skipping Snyk code test."
 fi
--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -4,9 +4,8 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 # Check for the presence of infrastructure as code files
-if find . \( -name '*.tf' -o -name '*.hcl' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' \) -print -quit &>/dev/null; then
+if find . \( -name '*.tf' -o -name '*.hcl' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.tfvars' -o -name '*.tfstate' -o -name '*.tfstate.backup' -o -name '*.tf.json' \) -print -quit &>/dev/null; then
   snyk iac test "$@"
 else
-  echo "Warning: No infrastructure as code files detected. Skipping Snyk IaC test."
+  echo "Warning: No supported infrastructure as code files detected. Skipping Snyk IaC test."
 fi
-
