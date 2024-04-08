27c950df96cb9bf24f069a582aa6069a4c5d7022
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-08 10:14:19 +0100

    Modifying snyk hooks to pass if valid files are not found

--- hooks/snyk-iac.sh
+++ hooks/snyk-iac.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 set +e
-snyk_exist_code=$(snyk iac test ...)
+snyk_exist_code=$(snyk iac test)
 if [ "$snyk_exist_code" = 2 ] || [ "$snyk_exist_code" = 3 ]; then
   echo "No supported projects detected"
   exit 0
