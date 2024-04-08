8e6ea898dc20d0f49e1ed5f11e62cff272bbceb3
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-08 11:56:08 +0100

    Modifying snyk hooks to pass if valid files are not found

--- hooks/snyk-code.sh
+++ hooks/snyk-code.sh
@@ -9,10 +9,16 @@ snyk_exit_code=$?
 set -e
 
 # Check if the exit code is 2
-if [ "$snyk_exit_code" = 2 ] || [ "$snyk_exit_code" = 3 ]; then
+if [ "$snyk_exit_code" = 2 ]; then
+  echo "Error, try running manually, use -d to output debug logs"
+  exit 0
+fi
+
+if [ "$snyk_exit_code" = 3 ]; then
   echo "Valid files not found."
   exit 0
 fi
+
 
 # If the exit code is not 2, exit with the same code
 exit "$snyk_exit_code"
