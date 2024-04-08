ef5c282ffbf01a74665a37d411497254be316893
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-08 10:59:25 +0100

    Modifying snyk hooks to pass if valid files are not found

--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -9,9 +9,9 @@ bash "${SCRIPT_DIR}"/installation/main.sh
 snyk test "$@"
 snyk_exit_code=$?
 
-set +e
+#set +e
 if [ "$snyk_exit_code" = 2 ] || [ "$snyk_exit_code" = 3 ]; then
   echo "No supported projects detected"
   exit 0
 fi
-set -e
+#set -e
