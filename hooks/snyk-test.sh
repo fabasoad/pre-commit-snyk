cab5ef4704a557c36a9d944ea6e857e653e09ef2
Kieharper <kieran.harper@hotmail.co.uk>
2024-04-08 10:46:41 +0100

    Modifying snyk hooks to pass if valid files are not found

--- hooks/snyk-test.sh
+++ hooks/snyk-test.sh
@@ -7,11 +7,11 @@ SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
 set +e
-snyk_exist_code=$(snyk test)
-if [ "$snyk_exist_code" = 0 ]; then
+snyk_exit_code=$(snyk test)
+if [ "$snyk_exit_code" = 0 ]; then
   echo "No vulnerabilities found"
   exit 0
 else
   echo "Vulnerabilities found"
-  exit "$snyk_exist_code"
+  exit "$snyk_exit_code"
 fi
