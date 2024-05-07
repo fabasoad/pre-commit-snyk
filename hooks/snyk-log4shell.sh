1bb38a5749d1ea8ce371cf409f13fbfaba1ec1fd
Kieharper <kieran.harper@hotmail.co.uk>
2024-05-07 08:54:07 +0100

    Modifying snyk hooks to pass if valid files are not found

--- hooks/snyk-log4shell.sh
+++ hooks/snyk-log4shell.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-set -eu
+set -u
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 bash "${SCRIPT_DIR}"/installation/main.sh
 
+# Capture exit code of Snyk Test hook
+set +e
 snyk log4shell "$@"
+snyk_exit_code=$?
+set -e
+
+# Check if the exit code is 3
+if [ "$snyk_exit_code" = 3 ]; then
+  exit 0
+fi
+
+exit "$snyk_exit_code"
