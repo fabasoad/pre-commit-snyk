c83ceff1f646595aedc6bd41db30719489aaa678
Kieharper <kieran.harper@hotmail.co.uk>
2024-05-07 08:59:18 +0100

    Modifying snyk hooks to pass if valid files are not found

--- hooks/installation/main.sh
+++ hooks/installation/main.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+# shellcheck disable=SC2039,SC3020
 set -eu
 
-if ! command -v snyk &> /dev/null
-then
-  SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+SCRIPT_PATH=$(realpath "$0")
+INSTALLATION_FOLDER_PATH=$(dirname "${SCRIPT_PATH}")
+
+if ! command -v snyk &> /dev/null; then
   if command -v brew &> /dev/null; then
-    bash "${SCRIPT_DIR}"/install-brew.sh
+    sh "${INSTALLATION_FOLDER_PATH}/install-brew.sh"
   elif command -v scoop &> /dev/null; then
-    bash "${SCRIPT_DIR}"/install-scoop.sh
+    sh "${INSTALLATION_FOLDER_PATH}/install-scoop.sh"
   elif command -v npm &> /dev/null; then
-    bash "${SCRIPT_DIR}"/install-npm.sh
+    sh "${INSTALLATION_FOLDER_PATH}/install-npm.sh"
   elif command -v yarn &> /dev/null; then
-    bash "${SCRIPT_DIR}"/install-yarn.sh
+    sh "${INSTALLATION_FOLDER_PATH}/install-yarn.sh"
   else
-    bash "${SCRIPT_DIR}"/install-standalone.sh
+    sh "${INSTALLATION_FOLDER_PATH}/install-standalone.sh"
   fi
 fi
