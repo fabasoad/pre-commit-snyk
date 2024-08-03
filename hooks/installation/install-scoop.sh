258c296b3139869e4020839d7e190e8af45172f6
fabasoad <fabasoad@gmail.com>
2024-08-04 07:33:56 +0900

    Debug 2

--- hooks/installation/install-scoop.sh
+++ hooks/installation/install-scoop.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
+echo "~ Scoop version:"
 scoop --version
+echo "~ Windows version:"
 cmd /c ver
+echo "~~~~"
 scoop bucket add snyk https://github.com/snyk/scoop-snyk
 scoop install snyk
