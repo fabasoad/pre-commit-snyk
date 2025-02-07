2607bcdea1e32188c0777781d36293f7858afbb7
Eugene <fabasoad@gmail.com>
2025-02-07 23:58:23 +0900

    Add bash version validation (#54)
    Add bash version validation

--- src/lib/utils/validators.sh
+++ src/lib/utils/validators.sh
@@ -25,3 +25,15 @@ validate_semver() {
 validate_tool_installed() {
   _run_validator "fabasoad_validate_tool_installed" "$@"
 }
+
+validate_bash_version() {
+  minimum_required_major_version=4
+  bash_version=$(bash --version | head -n 1 | cut -d ' ' -f 4)
+  bash_major=$(echo "${bash_version}" | cut -d '.' -f 1)
+  if [ "${bash_major}" -lt "${minimum_required_major_version}" ]; then
+    msg="bash version ${minimum_required_major_version}.0 or higher is required."
+    msg="${msg} Current version is ${bash_version}."
+    fabasoad_log "error" "${msg}"
+    exit 1
+  fi
+}
